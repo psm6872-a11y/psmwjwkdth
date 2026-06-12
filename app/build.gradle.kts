e80bb09e5973f84d64592a231f900e1af3ffe87d
Danalla Calendar Developer <danalla-calendar-developer@example.com>
2026-06-12 21:20:54 +0900

    버전 업그레이드: 1.3.38 (code 184)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.danallacalendar"
         minSdk = 24
         targetSdk = 34
-        versionCode = 183
-        versionName = "1.3.37"
+        versionCode = 184
+        versionName = "1.3.38"
         val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
         if (localPropertiesFile.exists()) {
