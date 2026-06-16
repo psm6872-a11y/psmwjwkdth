df3830796b0034a132598f99a34b2b8702937836
Danalla Calendar Developer <danalla-calendar-developer@example.com>
2026-06-16 21:28:04 +0900

    Bump version to 1.6.02 (252)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.danalla.calendar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 251
-        versionName = "1.6.01"
+        versionCode = 252
+        versionName = "1.6.02"
     }
 
     signingConfigs {
