74614a61e10fe9934cf515542650f35c20941222
Danalla Calendar Developer <danalla-calendar-developer@example.com>
2026-06-25 02:34:23 +0900

    bump: version update to v1.7.27 (312) upon user request

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.danalla.calendar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 311
-        versionName = "1.7.26"
+        versionCode = 312
+        versionName = "1.7.27"
     }
 
     signingConfigs {
