09f89e40483d293bf389fe9a7ae35880c595bd61
Danalla Calendar Developer <danalla-calendar-developer@example.com>
2026-06-28 14:43:19 +0900

    v1.7.37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.danalla.calendar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 321
-        versionName = "1.7.36"
+        versionCode = 322
+        versionName = "1.7.37"
     }
 
     signingConfigs {
