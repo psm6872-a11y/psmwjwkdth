8c4d7fb83799c4426cebac5e713f49dd3cbf7ce9
Danalla Calendar Developer <danalla-calendar-developer@example.com>
2026-07-30 16:38:20 +0900

    bump: version 1.8.21 (390)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.danalla.calendar"
         minSdk = 24
         targetSdk = 36
-        versionCode = 389
-        versionName = "1.8.20"
+        versionCode = 390
+        versionName = "1.8.21"
     }
 
     signingConfigs {
